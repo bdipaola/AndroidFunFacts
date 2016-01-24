c1fdae68fb97a37c3e9de18b6552562eff3dfa23
[01;31m[K"name"[m[K: "Ben DiPaola <[01;31m[K"email"[m[K>
2016-01-24 10:48:55 -0600

    Update relative layout background color to red on click

--- app/src/main/java/bdipaola/funfacts/FunFactsActivity.java
+++ app/src/main/java/bdipaola/funfacts/FunFactsActivity.java
@@ -1,18 +1,19 @@
 package bdipaola.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
     private FactBook mFactBook = new FactBook();
     // Declare our view variables
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class FunFactsActivity extends AppCompatActivity {
         // Assign the Views from the layout file to the correspoinding variables
         mFactTextView = (TextView) findViewById(R.id.factTextView);
         mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
@@ -30,6 +32,7 @@ public class FunFactsActivity extends AppCompatActivity {
 
                 // Update the screen with our dynamic fact
                 mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(Color.RED);
             }
         };
         mShowFactButton.setOnClickListener(listener);
